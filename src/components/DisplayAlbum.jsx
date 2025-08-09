5b13b27b0b9058e83a750d500da6d8bb0ceecc35
Yudistiraa33 <inyomanyudistira33@gmail.com>
2025-08-09 22:35:17 +0800

    update display album

--- src/components/DisplayAlbum.jsx
+++ src/components/DisplayAlbum.jsx
@@ -28,6 +28,18 @@ const DisplayAlbum = () => {
           </p>
         </div>
     </div>
+    <div className='grid grid-cols-3 sm:grid-cols-4 mt-10 mb-4 pl-2 text-[#a7a7a7]'>
+      <p><b className='mr-4'>#</b>Title</p>
+      <p>Album</p>
+      <p className='hidden sm:block '>Date Added</p>
+      <img className='m-auto w-4' src={assets.clock_icon} alt="" />
+    </div>
+    <hr />
+    {
+      songsData.map((item,index) => (
+        <div className='grid grid-cols-3 sm:grid-cols-4 gap-2 p-2 items-center text-[#a7a7a7] hober:bg-[#ffffff2b] cursor-pointer'></div>
+      ))
+    }
     </>
   )
 }
