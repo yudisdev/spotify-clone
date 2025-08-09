295f88ccf554e3f589980dcf79fcd462f66695e4
Yudistiraa33 <inyomanyudistira33@gmail.com>
2025-08-09 19:27:56 +0800

    update display album

--- src/components/DisplayAlbum.jsx
+++ src/components/DisplayAlbum.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from './Navbar'
-import { albumsData } from '../assets/assets';
+import { albumsData, assets } from '../assets/assets';
 import { useParams } from 'react-router-dom';
 
 const DisplayAlbum = () => {
@@ -14,10 +14,22 @@ const DisplayAlbum = () => {
     <>
     <Navbar/>
     <div className='mt-10 flex gap-8 flex-col md:flex-row md:items-end'>
-        <img src={albumData.image} alt="" />
+        <img className='w-48 rounded' src={albumData.image} alt="" />
+        <div className='flex flex-col'>
+          <p>Playlist</p>
+          <h2 className='text-5xl font-bold mb-4 md:text-7xl'>{albumData.name}</h2>
+          <h4>{albumData.desc}</h4>
+          <p className='mt-1'>
+            <img className='inline-block w-5' src={assets.spotify_logo} alt="" />
+            <b>Spotify</b>
+            • 1,323,154 likes
+            • <b>50 songs,</b>
+            about 2 hr 30 min
+          </p>
+        </div>
     </div>
     </>
   )
 }
 
-export default DisplayAlbum
+export default DisplayAlbum;
