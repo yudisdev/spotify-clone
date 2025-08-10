554f331891b3b6eb764a62f783542f4c658aa681
Yudistiraa33 <inyomanyudistira33@gmail.com>
2025-08-10 19:12:44 +0800

    update fiture play and pause

--- src/context/PlayerContext.jsx
+++ src/context/PlayerContext.jsx
@@ -1,13 +1,50 @@
 import { createContext, useRef } from 'react';
+import { songsData } from '../assets/assets';
+import { useState } from 'react';
 
 export const PlayerContext = createContext();
 
 const PlayerContextProvider = (props) => {
 
     const audioRef = useRef();
+    const seekBg = useRef();
+    const seekBar = useRef();
+
+    const [track,setTrack] = useState(songsData[0]);
+    const [playStatus, setPlayStatus] = useState(false);
+    const [time,setTime] = useState({
+        currentTime: {
+            seconds: 0,
+            minutes: 0
+        },
+        totalTime:{
+            seconds: 0,
+            minutes: 0
+        }
+    })
+
+    const play = () => {
+        audioRef.current.play();
+        setPlayStatus(true);
+    }
+
+    const pause = () => {
+        audioRef.current.pause();
+        setPlayStatus(false);
+    }
+
+
 
     const contextValue = {
-        audioRef
+        audioRef,
+        seekBg,
+        seekBar,
+        track,
+        setTrack,
+        playStatus, setPlayStatus,
+        time, setTime,
+        play, pause
+
     }
 
     return (
