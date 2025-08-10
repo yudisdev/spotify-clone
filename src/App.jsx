158bf6d8c444846296459f82b95e932bd4e80ecc
Yudistiraa33 <inyomanyudistira33@gmail.com>
2025-08-10 18:09:59 +0800

    update main.jsx and app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Sidebar from './components/Sidebar'
 import Player from './components/Player'  
 import Display from './components/Display'
+import { PlayerContext } from './context/PlayerContext'
 
 
 const App = () => {
+
+  const {audioRef} = useContext(PlayerContext);
+
   return (
     <div className='h-screen bg-black'>
       <div className='h-[90%] flex'>
@@ -13,7 +17,7 @@ const App = () => {
      
       </div>
       <Player />
-      <audio preload ='auto'></audio>
+      <audio ref={audioRef} preload ='auto'></audio>
     </div>
   )
 }
